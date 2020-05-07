f6b1cc4eec5d4fb29d21d2c481439490f28db498
manu <manuel.regidor@sygel.es>
2020-05-07 11:09:11 +0200

    [ADD] l10n_es_aeat_mod303_print

---
+++ l10n_es_aeat_mod303_print/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "AEAT Modelo 303 Imprimir",
+    "summary": "PDF del model 303",
+    "version": "12.0.1.0.0",
+    "category": "Uncategorized",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_es_aeat_mod303",
+    ],
+    "data": [
+        "report/aeat_mod303.xml",
+        "report/report_views.xml",
+    ],
+}
